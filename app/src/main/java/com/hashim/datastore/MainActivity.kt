e00addeb380b2cb2c5b9e53c7501f0e1953ff714
hashim <hashimtahir@live.com>
2021-04-13 17:22:44 +0500

    Initial commit.

---
+++ app/src/main/java/com/hashim/datastore/MainActivity.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021/  4/ 13.  Created by Hashim Tahir
+ */
+
+package com.hashim.datastore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
